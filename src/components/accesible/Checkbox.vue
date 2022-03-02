ee53e699f57c97d5d292e3afe8c5c70c527a752c
mgr <koumgrars13@gmail.com>
2022-03-02 23:39:05 +0900

    [wip]checkbox

---
+++ src/components/accesible/Checkbox.vue
@@ -0,0 +1,77 @@
+<script setup lang="ts">
+const checkedList = ref([])
+const onSubmit = () => {
+  alert(`checked: ${checkedList.value}`)
+}
+</script>
+
+<template>
+  <section
+    id="checkbox-section"
+    class="border-solid border-1 border-gray-300"
+  >
+    <h3 class="sr-only">
+      チェックボックスのサンプル
+    </h3>
+    <div>
+      <form @submit.prevent="onSubmit">
+        <div class="block">
+          <input
+            id="tomato"
+            v-model="checkedList"
+            value="tomato"
+            type="checkbox"
+          >
+          <label
+            for="tomato"
+            class="ml-1"
+          >Tomato</label>
+        </div>
+        <div class="block">
+          <input
+            id="banana"
+            v-model="checkedList"
+            value="banana"
+            type="checkbox"
+          >
+          <label
+            for="banana"
+            class="ml-1"
+          >Banana</label>
+        </div>
+        <div class="block">
+          <input
+            id="onion"
+            v-model="checkedList"
+            value="onion"
+            type="checkbox"
+          >
+          <label
+            for="onion"
+            class="ml-1"
+          >Onion</label>
+        </div>
+        <div class="block">
+          <input
+            id="potato"
+            v-model="checkedList"
+            value="potato"
+            type="checkbox"
+          >
+          <label
+            for="potato"
+            class="ml-1"
+          >Potato</label>
+        </div>
+        <div class="block mt-2">
+          <button
+            type="submit"
+            class="px-4 py-1 bg-green-700 hover:bg-green-600 text-white rounded-lg"
+          >
+            送信
+          </button>
+        </div>
+      </form>
+    </div>
+  </section>
+</template>
---
+++ src/pages/accesible/checkbox.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import Checkbox from '@/components/accesible/Checkbox.vue'
+import AccesibleLayout from '@/components/AccesibleLayout.vue';
+</script>
+
+<template>
+  <AccesibleLayout>
+    <template #title>
+      accordion (TBD)
+    </template>
+    <template #section-title>
+      <Link
+        href="https://waic.jp/docs/2019/NOTE-wai-aria-practices-1.1-20190207/#checkbox"
+        :is-blank="true"
+        name="3.7 チェックボックス"
+      />
+    </template>
+    <template #example>
+      <div
+        id="checkbox-group"
+        class="w-64"
+      >
+        <Checkbox />
+      </div>
+    </template>
+    <template #memo>
+      <ul>
+        <li>TBD</li>
+      </ul>
+    </template>
+  </AccesibleLayout>
+</template>
